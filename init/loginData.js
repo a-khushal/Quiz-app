21086e494c2d4e78510fcaefa0d384e6c8ebb6e8
khushal <khushal2004a@gmail.com>
2023-12-19 18:11:43 +0530

    changes in email

--- init/loginData.js
+++ init/loginData.js
@@ -1,8 +1,8 @@
 const UserData = [
-    {email: "khushala.cy22@rvce.edu.in", password: "welcome@123"},
-    {email: "kishorea.cy22@rvce.edu.in", password: "welcome@123"},
-    {email: "deekshithv.cy22@rvce.edu.in", password: "welcome@123"},
-    {email: "dharmikjrai.cy22@rvce.edu.in", password: "welcome@123"},
+    {email: "khushala.cy22@gmail.com", password: "welcome@123"},
+    {email: "kishorea.cy22@gmail.com", password: "welcome@123"},
+    {email: "deekshithv.cy22@gmail.com", password: "welcome@123"},
+    {email: "dharmikjrai.cy22@gmail.com", password: "welcome@123"},
 ] 
 
 module.exports = {UserData};
