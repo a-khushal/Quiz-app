67100f3e2e6fc66dc68f816400e0bf46a6293dbe
khushal <khushal2004a@gmail.com>
2023-12-24 15:12:08 +0530

    changes in admin route

--- index.js
+++ index.js
@@ -90,7 +90,90 @@ app.get("/management/new", (req, res)=>{
     res.render("admin/addNew.ejs");
 });
 
+app.post("/management/new/teacher", async(req, res)=>{
+    try{
+        let {username, email, subjName, subjCode} =  req.body;
+        if(username && email && subjName && subjCode){
+            let password = 'welcome@123';
+            await teacherLoginDB.insertMany({
+                username: username, 
+                email: email, 
+                password: password, 
+                subject:{
+                    name: subjName, 
+                    code: subjCode,
+                }
+            });
+        res.send("new teacher added");
+        return;
+        }
+        res.send("all fields are mandatory!");
+        return;
+    }
+    catch(err){
+        console.log(err);
+        res.send("some error occured");
+    }
+});
 
+app.post("/management/new/student", async(req, res)=>{
+    try{
+        let {username, email, subjName1, subjCode1, subjName2, subjCode2, subjName3, subjCode3, subjName4, subjCode4, subjName5, subjCode5, subjName6, subjCode6, subjName7, subjCode7, subjName8, subjCode8, subjName9, subjCode9, subjName10, subjCode10} = req.body;
+        let password = 'welcome@123';
+        await studentLoginDB.insertMany({
+            username: username, 
+            email: email, 
+            password: password, 
+            subject:[
+                {
+                    name: subjName1, 
+                    code: subjCode1,
+                },
+                {
+                    name: subjName2, 
+                    code: subjCode2,
+                },
+                {
+                    name: subjName3, 
+                    code: subjCode3,
+                },
+                {
+                    name: subjName4, 
+                    code: subjCode4,
+                },
+                {
+                    name: subjName5, 
+                    code: subjCode5,
+                },
+                {
+                    name: subjName6, 
+                    code: subjCode6,
+                },
+                {
+                    name: subjName7, 
+                    code: subjCode7,
+                },
+                {
+                    name: subjName8, 
+                    code: subjCode8,
+                },
+                {
+                    name: subjName9, 
+                    code: subjCode9,
+                },
+                {
+                    name: subjName10, 
+                    code: subjCode10,
+                },
+            ]
+        });
+        res.send("new student added");
+        return;
+    } catch(err){
+        console.log(err);
+        res.send('some error');
+    }
+})
 
 app.get("/studentLogin/:id", async(req, res)=>{
     const {id} = req.params;
